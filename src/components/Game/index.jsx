1c893d6cc828cb1c2237d9299591cb72f0862f43
Caio <108128119+DocCaio@users.noreply.github.com>
2024-04-09 17:31:36 -0300

    up

---
+++ src/components/Game/index.jsx
@@ -0,0 +1,49 @@
+import './_Main.scss';
+
+export default function Card() {
+    return   (
+        <section className="container">
+
+          <div className="hangman-box">
+            <img src="{Zero}" alt='hangman-img'/>
+            <h1>Hangman Game</h1>
+          </div>
+          <div className='game-box'>
+            <ul className='word-display'>
+              <li className='letter'></li>
+              <li className='letter'></li>
+              <li className='letter'></li>
+              <li className='letter'></li>
+              <li className='letter'></li>
+              <li className='letter'></li>
+              <li className='letter'></li>
+            </ul>
+            <h4 className='hint-text'>
+              Hint:
+              <strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ea magni quo officiis nisi voluptatum reprehenderit dolorum alias eius repellendus! Voluptatum in eaque et non similique ab nobis voluptatem rerum.</strong>
+            </h4>
+            <h4 className='guesses-text'>
+              Incorrect guesses:
+              <strong>0 / 6</strong>
+            </h4>
+            <div className='keyboard'>
+              <button>a</button> <button>b</button> <button>c</button> <button>d</button>
+              <button>e</button> <button>f</button> <button>g</button> <button>h</button>
+              <button>i</button> <button>j</button> <button>k</button> <button>l</button>
+              <button>m</button> <button>n</button> <button>o</button> <button>p</button>
+              <button>q</button> <button>r</button> <button>s</button> <button>t</button>
+              <button>u</button> <button>v</button> <button>w</button> <button>x</button>
+              <button>y</button> <button>z</button>
+            </div>
+
+            
+          </div>
+        
+        </section>
+        
+
+    )
+       
+    
+
+}
